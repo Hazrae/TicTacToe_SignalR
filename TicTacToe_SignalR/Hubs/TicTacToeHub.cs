6ac5bd6315434fac827f0008c008d5201e25e438
IraVehementi <IraVehementi@DESKTOP-7ND5R6T>
2020-04-16 17:12:50 +0200

    Ajoutez des fichiers projet.

---
+++ TicTacToe_SignalR/Hubs/TicTacToeHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToe_SignalR.Hubs
+{
+    public class TicTacToeHub : Hub
+    {
+        public async Task SendMessage(string id, int cpt)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", id, cpt);
+        }
+    }
+}
---
+++ TicTacToe_SignalR/wwwroot/js/tictactoe.js
@@ -0,0 +1,53 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl('/tictactoeHub').build();
+var cptTour;
+
+
+
+document.querySelectorAll("td").forEach(td => td.addEventListener("click", function (event) {
+    var idElem= this.getAttribute('id');
+    connection.invoke("SendMessage", idElem, cptTour).catch(function (err) {
+        console.error(err.toString());
+    });
+    event.preventDefault();
+}));
+
+
+connection.start().then(function () {
+    console.log("Connecté!");   
+}).catch(function (err) {
+    console.error(err.toString());
+});
+
+connection.on("ReceiveMessage", function (id, cpt) {
+    var td = document.getElementById(id);
+    cptTour = cpt;
+
+    if (td.firstChild == null) {
+        if (cptTour % 2 == 0)
+            td.appendChild(document.createTextNode("X"));
+        else
+            td.appendChild(document.createTextNode("0"));
+        cptTour++;
+    }
+    else
+        alert("Case déjà jouée");
+})
+/*
+document.querySelectorAll("td").forEach(td => td.addEventListener("click", function () {
+
+    var td = document.getElementById(this.id);
+
+    if (td.firstChild == null) {
+        if (cptTour % 2 == 0)
+            td.appendChild(document.createTextNode("X"));
+        else
+            td.appendChild(document.createTextNode("0"));
+        cptTour++;
+    }
+    else
+        alert("Case déjà jouée");
+
+}));
+*/
