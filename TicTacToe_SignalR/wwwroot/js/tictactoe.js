49a176f6981bcbbbc21d1c04bd87b74ac8903025
IraVehementi <IraVehementi@DESKTOP-7ND5R6T>
2020-04-17 15:30:41 +0200

    OK tic tac toe

--- TicTacToe_SignalR/wwwroot/js/tictactoe.js
+++ TicTacToe_SignalR/wwwroot/js/tictactoe.js
@@ -3,17 +3,20 @@
 var connection = new signalR.HubConnectionBuilder().withUrl('/TicTacToeHub').build();
 var cptTour = 0;
 
-
 document.querySelectorAll("td").forEach(td => td.addEventListener("click", function (event) {   
+    if (td.firstChild == null) {
     connection.invoke("SendAction", this.id, cptTour).catch(function (err) {
-        console.error(err.toString());
+        console.error(err.toString())
     });
+    }
+    else
+        alert("Case déjà jouée");
     event.preventDefault();
 }));
 
-
 connection.start().then(function () {
-    console.log("Connecté!");   
+    console.log("Connecté!");
+    GetSymbol();
 }).catch(function (err) {
     console.error(err.toString());
 });
@@ -21,14 +24,32 @@ connection.start().then(function () {
 connection.on("PlaceToken", function (id, cpt) {
     var td = document.getElementById(id);
     cptTour = cpt;
-
-    if (td.firstChild == null) {
-        if (cptTour % 2 == 0)
-            td.appendChild(document.createTextNode("X"));
-        else
-            td.appendChild(document.createTextNode("0"));
-        cptTour++;
+        
+    if (cptTour % 2 == 0) {
+        let node = document.createElement("div");
+        node.style.fontSize = "30px";
+        node.style.fontWeight = "Bold";
+        node.style.color = "white";
+        node.innerHTML = "X";
+        td.appendChild(node);
     }
-    else
-        alert("Case déjà jouée");
+    else {
+        let node = document.createElement("div");
+        node.style.fontSize = "30px";
+        node.style.fontWeight = "Bold";
+        node.style.color = "white";
+        node.innerHTML = "0";
+        td.appendChild(node);
+    }
+        
+    cptTour++;
+    GetSymbol();
 })
+
+function GetSymbol() {
+    var node = document.getElementById("cptTour");
+    if (cptTour % 2 == 0)    
+        node.innerHTML = "Vous jouez avec le X";       
+    else     
+        node.innerHTML = "Vous jouez avec le 0";  
+}
